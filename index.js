68919d5a86f20090ec7e7bf16ed32453b905507d
prahal <vprahaladhan@gmail.com>
2021-02-26 15:02:37 +1100

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const writeCards = (names, message) => {
+  let messages = [];
+  names.forEach(name => {
+    messages.push(`Thank you, ${name}, for the wonderful ${message} gift!`)
+  });
+  return messages;
+}
+
+const countDown = number => {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
